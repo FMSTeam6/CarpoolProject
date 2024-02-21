7d258f45aadbe1054d1001ee1f992a34f82f429b
AKPatrashkov <sashko1006@abv.bg>
2024-02-21 19:27:46 +0200

    Initial commit

---
+++ src/test/java/com/finalproject/carpool/CarpoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.carpool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
